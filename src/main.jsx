5ad0611bb5ab8038163e5327e9c2001b29fa1a30
Oleksandr <125139066+oleksamor@users.noreply.github.com>
2024-08-28 14:16:06 +0300

    change mainjsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import React from "react";
+import "modern-normalize";
 import App from "./componenets/App";
 import "./index.css";
+import { Provider } from "react-redux";
+import { ReactDOM } from "react-dom/client";
 
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <Provider>
     <App />
-  </StrictMode>
+  </Provider>
 );
